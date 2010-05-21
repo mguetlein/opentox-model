c9c48a20e31f3ae71948a30347db6eaa0ff76b3d
Christoph Helma <helma@in-silico.ch>
2010-05-21 18:48:47 +0200

    opentox-api-wrapper bumped to

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-gem "opentox-ruby-api-wrapper", "= 1.5.4"
+gem "opentox-ruby-api-wrapper", "= "
 require 'opentox-ruby-api-wrapper'
 LOGGER.progname = File.expand_path(__FILE__)
 
