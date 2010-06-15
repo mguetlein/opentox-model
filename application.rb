351f6b0ee64259f1254635704c0edfa5a54b34a8
Christoph Helma <helma@in-silico.ch>
2010-06-15 17:17:51 +0200

    updated for new datamapper release

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-gem "opentox-ruby-api-wrapper", "= 1.5.5"
+gem "opentox-ruby-api-wrapper", "= 1.5.6"
 require 'opentox-ruby-api-wrapper'
 LOGGER.progname = File.expand_path(__FILE__)
 
