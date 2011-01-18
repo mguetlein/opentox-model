0fdf7ed7fb5b80c78aaa16f8c75ce11dc3840d6e
mr <mr@mrautenberg.de>
2011-01-18 11:29:42 +0100

    get subjectid from api-wrapper helper

--- application.rb
+++ application.rb
@@ -61,12 +61,11 @@ delete '/:id/?' do
     uri = ModelStore.get(params[:id]).uri
     ModelStore.get(params[:id]).destroy!
     "Model #{params[:id]} deleted."
-    subjectid = params[:subjectid] if params[:subjectid]
-    subjectid = request.env['HTTP_SUBJECTID'] if !subjectid and request.env['HTTP_SUBJECTID']		
-    if subjectid and !ModelStore.get(params[:id]) and uri
+
+    if @subjectid and !ModelStore.get(params[:id]) and uri
       begin
-        res = OpenTox::Authorization.delete_policies_from_uri(uri, subjectid)
-        LOGGER.debug "Policy deleted for Model URI: #{uri} with subjectid: #{subjectid} with result: #{res}"
+        res = OpenTox::Authorization.delete_policies_from_uri(uri, @subjectid)
+        LOGGER.debug "Policy deleted for Model URI: #{uri} with subjectid: #{@subjectid} with result: #{res}"
       rescue
         LOGGER.warn "Policy delete error for Model URI: #{uri}"
       end
